c9e5f3bfb31592813422520e444b9cbb50c7027b
Kelreel <derodroll@gmail.com>
2020-10-29 23:51:47 +0300

    init task view tabs

--- web-client/src/app/components/tasks/tasks-list/tasks-list.component.ts
+++ web-client/src/app/components/tasks/tasks-list/tasks-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {TaskService} from '../../../services/task.service';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { TaskService } from "../../../services/task.service";
+import { AuthService } from "src/app/services/auth.service";
 
 export interface Task {
   _id: string;
@@ -14,18 +14,30 @@ export interface Task {
 }
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.scss']
+  selector: "app-tasks-list",
+  templateUrl: "./tasks-list.component.html",
+  styleUrls: ["./tasks-list.component.scss"],
 })
 export class TasksListComponent implements OnInit {
   tasks: Task[];
-  constructor(private _tasksService: TaskService, public authService: AuthService) { }
+  constructor(
+    private _tasksService: TaskService,
+    public authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this._tasksService.getTasks().subscribe(data => {
+    this._tasksService.getTasks().subscribe((data) => {
       this.tasks = data;
+      this.tasks = this.tasks.map((task) => {
+        let content = task.content;
+        if (content.length > 40) {
+          content = content.substring(0, 40) + "...";
+        }
+        return {
+          ...task,
+          content,
+        };
+      });
     });
   }
-
 }
--- web-client/src/app/shared/material/material.module.ts
+++ web-client/src/app/shared/material/material.module.ts
@@ -1,17 +1,18 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
   declarations: [],
@@ -29,6 +30,8 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     MatSnackBarModule,
     MatSlideToggleModule,
     MatToolbarModule,
+    MatDividerModule,
+    MatTabsModule
   ],
   exports: [
     MatToolbarModule,
@@ -42,7 +45,9 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
     MatDialogModule,
     MatSnackBarModule,
     MatSlideToggleModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDividerModule,
+    MatTabsModule
   ],
 })
 export class MaterialModule {}
