24ce7b1d0c12c60b4fb30720ad6c4c05f5320c63
Kelreel <derodroll@gmail.com>
2020-11-13 12:39:14 +0300

    solve task alerts

--- react-app/src/components/Tasks/TaskView.tsx
+++ react-app/src/components/Tasks/TaskView.tsx
@@ -10,6 +10,7 @@ import {
   Theme,
   Typography,
 } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert/Alert";
 import Skeleton from "@material-ui/lab/Skeleton/Skeleton";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -69,22 +70,26 @@ const Task = () => {
 
   const onFlagSubmit = async (data: FormFlagData) => {
     setSubmitting(true);
+    setResult({ ...result, error: "" });
     try {
       let res = await TaskService.sendFlag(id, data.flag);
+      setResult({...result, correct: res.correct, score: res.score})
       setSubmitting(false);
     } catch (error) {
-      console.log(error.response?.data?.message);
+      setResult({ ...result, error: error.response?.data?.error });
     } finally {
       setSubmitting(false);
     }
   };
 
-  let skeleton = Array.from(Array(10)).map((i, ind) => (
+  const [result, setResult] = useState({ correct: false, score: 0, error: "" });
+
+  let skeleton = Array.from(Array(7)).map((i, ind) => (
     <Skeleton key={ind} variant="text" />
   ));
 
   return (
-    <Grid item xs={12} sm={8}>
+    <Grid item xs={12} sm={8} style={{ maxWidth: "400px" }}>
       <Paper className={classes.paper}>
         {isLoading ? (
           <>{skeleton}</>
@@ -101,7 +106,15 @@ const Task = () => {
             </Typography>
           </>
         )}
-        <Divider />
+        <Divider style={{ marginBottom: "1rem" }} />
+        {result.correct && (
+          <Alert severity="success">Success! +{result.score} points</Alert>
+        )}
+        {formState.isSubmitted &&
+          (result.error || !isLoading) &&
+          !result.correct && (
+            <Alert severity="error">{result.error || "Wrong flag"}</Alert>
+          )}
         <form className={classes.form} onSubmit={handleSubmit(onFlagSubmit)}>
           <TextField
             variant="outlined"
@@ -116,11 +129,11 @@ const Task = () => {
             variant="contained"
             style={{ marginLeft: "7px" }}
             color="primary"
-            disabled={!formState.isValid}
+            disabled={!formState.isValid || isLoading}
             onClick={handleSubmit(onFlagSubmit)}
           >
             {isSubmiting ? (
-              <CircularProgress color="secondary" size={15} />
+              <CircularProgress color="secondary" size={25} />
             ) : (
               `Submit`
             )}
